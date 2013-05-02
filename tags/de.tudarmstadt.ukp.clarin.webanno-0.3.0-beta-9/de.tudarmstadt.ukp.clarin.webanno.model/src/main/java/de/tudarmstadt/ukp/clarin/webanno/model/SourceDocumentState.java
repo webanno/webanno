3f5756c801edf52acce793721877a0a7dbe03bc1
seidymam <seidymam@8e34de24-5c2a-f3fb-db31-69dae2fc1fa9>
2013-05-02 08:41:34 +0000

    [maven-release-plugin]  copy for tag de.tudarmstadt.ukp.clarin.webanno-0.3.0-beta-9

---
+++ tags/de.tudarmstadt.ukp.clarin.webanno-0.3.0-beta-9/de.tudarmstadt.ukp.clarin.webanno.model/src/main/java/de/tudarmstadt/ukp/clarin/webanno/model/SourceDocumentState.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Copyright 2012
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package de.tudarmstadt.ukp.clarin.webanno.model;
+
+/**
+ * Variables for the different states of a {@link SourceDocument} workflow.
+ *
+ * @author Seid Muhie Yimam
+ *
+ */
+public enum SourceDocumentState
+{
+
+    /**
+     * no annotation document has been created for this document
+     */
+    NEW,
+    /**
+     * at least one annotation document has been created for the document
+     */
+    ANNOTATION_INPROGRESS,
+    /**
+     * all annotations have marked their annotation document as finished
+     */
+    ANNOTATION_FINISHED,
+    /**
+     * curator claims to have curated all annotations
+     */
+    CURATION_FINISHED,
+    /**
+     * curator has started working with the annotation document, annotators can no longer make
+     * modifications in annotation documents
+     */
+    CURATION_INPROGRESS;
+
+}
