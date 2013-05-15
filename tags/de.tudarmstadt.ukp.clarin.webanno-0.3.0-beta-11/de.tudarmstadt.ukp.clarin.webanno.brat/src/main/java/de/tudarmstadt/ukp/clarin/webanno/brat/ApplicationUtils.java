14eff1f730aaea922e4c2473182ef39ecf79e48d
seidymam <seidymam@8e34de24-5c2a-f3fb-db31-69dae2fc1fa9>
2013-05-15 16:01:53 +0000

    [maven-release-plugin]  copy for tag de.tudarmstadt.ukp.clarin.webanno-0.3.0-beta-11

---
+++ tags/de.tudarmstadt.ukp.clarin.webanno-0.3.0-beta-11/de.tudarmstadt.ukp.clarin.webanno.brat/src/main/java/de/tudarmstadt/ukp/clarin/webanno/brat/ApplicationUtils.java
@@ -0,0 +1,222 @@
+/*******************************************************************************
+ * Copyright 2012
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package de.tudarmstadt.ukp.clarin.webanno.brat;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+import javax.persistence.NoResultException;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.codehaus.jackson.JsonGenerator;
+import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
+
+import de.tudarmstadt.ukp.clarin.webanno.api.RepositoryService;
+import de.tudarmstadt.ukp.clarin.webanno.model.Authority;
+import de.tudarmstadt.ukp.clarin.webanno.model.PermissionLevel;
+import de.tudarmstadt.ukp.clarin.webanno.model.Project;
+import de.tudarmstadt.ukp.clarin.webanno.model.ProjectPermission;
+import de.tudarmstadt.ukp.clarin.webanno.model.User;
+
+public class ApplicationUtils
+{
+
+    private static MappingJacksonHttpMessageConverter jsonConverter;
+
+    public static void setJsonConverter(MappingJacksonHttpMessageConverter aJsonConverter)
+    {
+        jsonConverter = aJsonConverter;
+    }
+
+    private static final Log LOG = LogFactory.getLog(ApplicationUtils.class);
+
+    /**
+     * Read Tag and Tag Description. A line has a tag name and a tag description separated by a TAB
+     *
+     */
+    public static Map<String, String> getTagSetFromFile(String aLineSeparatedTags)
+    {
+        Map<String, String> tags = new LinkedHashMap<String, String>();
+        StringTokenizer st = new StringTokenizer(aLineSeparatedTags, "\n");
+        while (st.hasMoreTokens()) {
+            StringTokenizer stTag = new StringTokenizer(st.nextToken(), "\t");
+            String tag = stTag.nextToken();
+            String description;
+            if (stTag.hasMoreTokens()) {
+                description = stTag.nextToken();
+            }
+            else {
+                description = tag;
+            }
+            tags.put(tag.trim(), description);
+        }
+        return tags;
+    }
+
+    /**
+     * IS user super Admin
+     */
+    public static boolean isSuperAdmin(RepositoryService aProjectRepository, User aUser)
+    {
+        boolean roleAdmin = false;
+        List<Authority> authorities = aProjectRepository.getAuthorities(aUser);
+        for (Authority authority : authorities) {
+            if (authority.getRole().equals("ROLE_ADMIN")) {
+                roleAdmin = true;
+                break;
+            }
+        }
+        return roleAdmin;
+    }
+
+    /**
+     * Determine if the User is allowed to update a project
+     *
+     * @param aProject
+     * @return
+     */
+    public static boolean isProjectAdmin(Project aProject, RepositoryService aProjectRepository,
+            User aUser)
+    {
+        boolean roleAdmin = false;
+        List<Authority> authorities = aProjectRepository.getAuthorities(aUser);
+        for (Authority authority : authorities) {
+            if (authority.getRole().equals("ROLE_ADMIN")) {
+                roleAdmin = true;
+                break;
+            }
+        }
+
+        boolean projectAdmin = false;
+        if (!roleAdmin) {
+
+            try {
+                List<ProjectPermission> permissionLevels = aProjectRepository.listProjectPermisionLevel(aUser, aProject);
+                    for(ProjectPermission permissionLevel:permissionLevels) {
+                        if(StringUtils.equalsIgnoreCase(permissionLevel.getLevel().getName(),
+                                PermissionLevel.ADMIN.getName())) {
+                            projectAdmin = true;
+                            break;
+                        }
+                    }
+            }
+            catch (NoResultException ex) {
+                LOG.info("No permision is given to this user " + ex);
+            }
+        }
+
+        return (projectAdmin || roleAdmin);
+    }
+
+    /**
+     * Determine if the User is a curator or not
+     *
+     * @param aProject
+     * @return
+     */
+    public static boolean isCurator(Project aProject, RepositoryService aProjectRepository,
+            User aUser)
+    {
+        boolean roleAdmin = false;
+        List<Authority> authorities = aProjectRepository.getAuthorities(aUser);
+        for (Authority authority : authorities) {
+            if (authority.getRole().equals("ROLE_ADMIN")) {
+                roleAdmin = true;
+                break;
+            }
+        }
+
+        boolean curator = false;
+        if (!roleAdmin) {
+
+            try {
+                List<ProjectPermission> permissionLevels = aProjectRepository.listProjectPermisionLevel(aUser, aProject);
+                    for(ProjectPermission permissionLevel:permissionLevels) {
+                        if(StringUtils.equalsIgnoreCase(permissionLevel.getLevel().getName(),
+                                PermissionLevel.CURATOR.getName())) {
+                            curator = true;
+                            break;
+                        }
+                    }
+            }
+            catch (NoResultException ex) {
+                LOG.info("No permision is given to this user " + ex);
+            }
+        }
+
+        return (curator || roleAdmin);
+    }
+
+    /**
+     * Determine if the User is member of a project
+     *
+     * @param aProject
+     * @return
+     */
+    public static boolean isMember(Project aProject, RepositoryService aProjectRepository,
+            User aUser)
+    {
+        boolean roleAdmin = false;
+        List<Authority> authorities = aProjectRepository.getAuthorities(aUser);
+        for (Authority authority : authorities) {
+            if (authority.getRole().equals("ROLE_ADMIN")) {
+                roleAdmin = true;
+                break;
+            }
+        }
+
+        boolean user = false;
+        if (!roleAdmin) {
+
+            try {
+                List<ProjectPermission> permissionLevels = aProjectRepository.listProjectPermisionLevel(aUser, aProject);
+                    for(ProjectPermission permissionLevel:permissionLevels) {
+                        if(StringUtils.equalsIgnoreCase(permissionLevel.getLevel().getName(),
+                                PermissionLevel.USER.getName())) {
+                            user = true;
+                            break;
+                        }
+                    }
+            }
+
+            catch (NoResultException ex) {
+                LOG.info("No permision is given to this user " + ex);
+            }
+        }
+
+        return (user || roleAdmin);
+    }
+
+    public static void generateJson(Object aResponse, File aFile)
+            throws IOException
+        {
+            StringWriter out = new StringWriter();
+
+            JsonGenerator jsonGenerator = jsonConverter.getObjectMapper().getJsonFactory()
+                    .createJsonGenerator(out);
+
+            jsonGenerator.writeObject(aResponse);
+            FileUtils.writeStringToFile(aFile, out.toString());
+        }
+}
