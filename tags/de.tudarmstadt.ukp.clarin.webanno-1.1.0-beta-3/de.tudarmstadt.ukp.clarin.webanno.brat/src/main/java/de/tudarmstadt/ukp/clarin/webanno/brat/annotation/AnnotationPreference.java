a072f890611d49c0e4ed0ea538fa8d2e4fae76e1
seidymam <seidymam@8e34de24-5c2a-f3fb-db31-69dae2fc1fa9>
2014-01-20 13:40:50 +0000

    [maven-release-plugin]  copy for tag de.tudarmstadt.ukp.clarin.webanno-1.1.0-beta-3

---
+++ tags/de.tudarmstadt.ukp.clarin.webanno-1.1.0-beta-3/de.tudarmstadt.ukp.clarin.webanno.brat/src/main/java/de/tudarmstadt/ukp/clarin/webanno/brat/annotation/AnnotationPreference.java
@@ -0,0 +1,299 @@
+/*******************************************************************************
+ * Copyright 2012
+ * Ubiquitous Knowledge Processing (UKP) Lab and FG Language Technology
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package de.tudarmstadt.ukp.clarin.webanno.brat.annotation;
+
+import java.util.ArrayList;
+
+import de.tudarmstadt.ukp.clarin.webanno.model.TagSet;
+
+/**
+ * This is a class representing the bean objects to store users preference of annotation settings
+ * such as annotation layers, number of sentence to display at a time, visibility of lemma and
+ * whether to allow auto page scrolling.
+ *
+ * @author Seid Muhie Yimam
+ */
+public class AnnotationPreference
+{
+    // Id of annotation layers, to be stored in the properties file comma separated: 12, 34,....
+    private ArrayList<Long> annotationLayers;
+    private int windowSize = 10;
+    private boolean scrollPage;
+
+    private boolean predictInThisPage;
+    private boolean useExistingModel;
+    private long trainLayer = -1;
+    private long featureTagSet = -1;
+
+    private boolean capitalized;
+    private boolean containsNumber = true;
+
+    private boolean prefix1 = true;
+    private boolean prefix2 = true;
+    private boolean prefix3 = true;
+    private boolean prefix4 = true;
+    private boolean prefix5 = true;
+
+    private boolean suffix1 = true;
+    private boolean suffix2 = true;
+    private boolean suffix3 = true;
+    private boolean suffix4 = true;
+    private boolean suffix5 = true;
+
+    private int ngram = 3;
+    private int bigram;
+
+    /**
+     * Determine if MIRA automation is applicable to the annotator/correction view and automation view too
+     */
+    private boolean predictAnnotator;
+    private boolean predictAutomator;
+
+    public ArrayList<Long> getAnnotationLayers()
+    {
+        return annotationLayers;
+    }
+
+    public void setAnnotationLayers(ArrayList<Long> aAnnotationLayers)
+    {
+        annotationLayers = aAnnotationLayers;
+    }
+
+    public int getWindowSize()
+    {
+        return windowSize;
+    }
+
+    public void setWindowSize(int aWindowSize)
+    {
+        windowSize = aWindowSize;
+    }
+
+    public boolean isScrollPage()
+    {
+        return scrollPage;
+    }
+
+    public void setScrollPage(boolean aScrollPage)
+    {
+        scrollPage = aScrollPage;
+    }
+
+    public boolean isPredictInThisPage()
+    {
+        return predictInThisPage;
+    }
+
+    public void setPredictInThisPage(boolean predictInThisPage)
+    {
+        this.predictInThisPage = predictInThisPage;
+    }
+
+    public boolean isUseExistingModel()
+    {
+        return useExistingModel;
+    }
+
+    public void setUseExistingModel(boolean useExistingModel)
+    {
+        this.useExistingModel = useExistingModel;
+    }
+
+    public long getTrainLayer()
+    {
+        return trainLayer;
+    }
+
+    public void setTrainLayer(long trainLayerId)
+    {
+        this.trainLayer = trainLayerId;
+    }
+
+    
+    public boolean isCapitalized()
+    {
+        return capitalized;
+    }
+
+    public long getFeatureTagSet()
+    {
+        return featureTagSet;
+    }
+
+    public void setFeatureTagSet(long featureTagSet)
+    {
+        this.featureTagSet = featureTagSet;
+    }
+
+    public void setCapitalized(boolean capitalized)
+    {
+        this.capitalized = capitalized;
+    }
+
+    public boolean isContainsNumber()
+    {
+        return containsNumber;
+    }
+
+    public void setContainsNumber(boolean containsNumber)
+    {
+        this.containsNumber = containsNumber;
+    }
+
+    public boolean isPrefix1()
+    {
+        return prefix1;
+    }
+
+    public void setPrefix1(boolean prefix1)
+    {
+        this.prefix1 = prefix1;
+    }
+
+    public boolean isPrefix2()
+    {
+        return prefix2;
+    }
+
+    public void setPrefix2(boolean prefix2)
+    {
+        this.prefix2 = prefix2;
+    }
+
+    public boolean isPrefix3()
+    {
+        return prefix3;
+    }
+
+    public void setPrefix3(boolean prefix3)
+    {
+        this.prefix3 = prefix3;
+    }
+
+    public boolean isPrefix4()
+    {
+        return prefix4;
+    }
+
+    public void setPrefix4(boolean prefix4)
+    {
+        this.prefix4 = prefix4;
+    }
+
+    public boolean isPrefix5()
+    {
+        return prefix5;
+    }
+
+    public void setPrefix5(boolean prefix5)
+    {
+        this.prefix5 = prefix5;
+    }
+
+    public boolean isSuffix1()
+    {
+        return suffix1;
+    }
+
+    public void setSuffix1(boolean suffix1)
+    {
+        this.suffix1 = suffix1;
+    }
+
+    public boolean isSuffix2()
+    {
+        return suffix2;
+    }
+
+    public void setSuffix2(boolean suffix2)
+    {
+        this.suffix2 = suffix2;
+    }
+
+    public boolean isSuffix3()
+    {
+        return suffix3;
+    }
+
+    public void setSuffix3(boolean suffix3)
+    {
+        this.suffix3 = suffix3;
+    }
+
+    public boolean isSuffix4()
+    {
+        return suffix4;
+    }
+
+    public void setSuffix4(boolean suffix4)
+    {
+        this.suffix4 = suffix4;
+    }
+
+    public boolean isSuffix5()
+    {
+        return suffix5;
+    }
+
+    public void setSuffix5(boolean suffix5)
+    {
+        this.suffix5 = suffix5;
+    }
+
+    public int getNgram()
+    {
+        return ngram;
+    }
+
+    public void setNgram(int ngram)
+    {
+        this.ngram = ngram;
+    }
+
+    public int getBigram()
+    {
+        return bigram;
+    }
+
+    public void setBigram(int bigram)
+    {
+        this.bigram = bigram;
+    }
+
+    public boolean isPredictAnnotator()
+    {
+        return predictAnnotator;
+    }
+
+    public void setPredictAnnotator(boolean predictAnnotator)
+    {
+        this.predictAnnotator = predictAnnotator;
+    }
+
+    public boolean isPredictAutomator()
+    {
+        return predictAutomator;
+    }
+
+    public void setPredictAutomator(boolean predictAutomator)
+    {
+        this.predictAutomator = predictAutomator;
+    }
+
+
+}
