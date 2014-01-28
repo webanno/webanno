f3b7018253ccd8e09755d7c58fcb0c6198601e53
seidymam <seidymam@8e34de24-5c2a-f3fb-db31-69dae2fc1fa9>
2014-01-28 21:40:11 +0000

    [maven-release-plugin]  copy for tag de.tudarmstadt.ukp.clarin.webanno-1.1.0-beta-10

---
+++ tags/de.tudarmstadt.ukp.clarin.webanno-1.1.0-beta-10/de.tudarmstadt.ukp.clarin.webanno.webapp/src/main/java/de/tudarmstadt/ukp/clarin/webanno/webapp/page/annotation/component/ExportModalPanel.java
@@ -0,0 +1,90 @@
+/*******************************************************************************
+ * Copyright 2012
+ * Ubiquitous Knowledge Processing (UKP) Lab and FG Language Technology
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package de.tudarmstadt.ukp.clarin.webanno.webapp.page.annotation.component;
+
+import org.apache.wicket.Page;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
+
+import de.tudarmstadt.ukp.clarin.webanno.brat.annotation.BratAnnotatorModel;
+import de.tudarmstadt.ukp.clarin.webanno.webapp.dialog.ExportModalWindowPage;
+import de.tudarmstadt.ukp.clarin.webanno.webapp.page.annotation.AnnotationPage;
+import de.tudarmstadt.ukp.clarin.webanno.webapp.page.correction.CorrectionPage;
+import de.tudarmstadt.ukp.clarin.webanno.webapp.page.curation.CurationPage;
+
+/**
+ * A panel used for {@link AnnotationPage} {@link CurationPage} and {@link CorrectionPage}
+ * consisting of a link to export annotated data
+ *
+ * @author Seid Muhie Yimam
+ *
+ */
+public class ExportModalPanel
+    extends Panel
+{
+    private static final long serialVersionUID = 671214149298791793L;
+
+    public ExportModalPanel(String id, final IModel<BratAnnotatorModel> aModel)
+    {
+        super(id, aModel);
+
+        final ModalWindow exportModal;
+        add(exportModal = new ModalWindow("exportModal"));
+
+        exportModal.setCookieName("modal-1");
+        exportModal.setInitialWidth(550);
+        exportModal.setInitialHeight(450);
+        exportModal.setResizable(true);
+        exportModal.setWidthUnit("px");
+        exportModal.setHeightUnit("px");
+        exportModal.setTitle("Export Annotated data to a given Format");
+
+        exportModal.setPageCreator(new ModalWindow.PageCreator()
+        {
+            private static final long serialVersionUID = -2827824968207807739L;
+
+            @Override
+            public Page createPage()
+            {
+                return new ExportModalWindowPage(exportModal, aModel.getObject());
+            }
+
+        });
+        add(new AjaxLink<Void>("showExportModal")
+        {
+            private static final long serialVersionUID = 7496156015186497496L;
+
+            @Override
+            public void onClick(AjaxRequestTarget target)
+            {
+                if (aModel.getObject().getDocument() == null) {
+                    target.appendJavaScript("alert('Please open a document first!')");
+                }
+                else {
+                    exportModal.show(target);
+                }
+
+            }
+        });
+
+    }
+
+}
