f7d7e73b835da4f3f04e04b653ec4bf43060297c
seidymam <seidymam@8e34de24-5c2a-f3fb-db31-69dae2fc1fa9>
2014-06-10 12:38:59 +0000

    [maven-release-plugin]  copy for tag de.tudarmstadt.ukp.clarin.webanno-1.2.x-beta-3

---
+++ tags/de.tudarmstadt.ukp.clarin.webanno-1.2.x-beta-3/webanno.model/src/main/java/de/tudarmstadt/ukp/clarin/webanno/model/SourceDocumentState.java
@@ -0,0 +1,72 @@
+/*******************************************************************************
+ * Copyright 2012
+ * Ubiquitous Knowledge Processing (UKP) Lab and FG Language Technology
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package de.tudarmstadt.ukp.clarin.webanno.model;
+
+/**
+ * Variables for the different states of a {@link SourceDocument} workflow.
+ *
+ * @author Seid Muhie Yimam
+ *
+ */
+public enum SourceDocumentState implements PersistentEnum
+{
+
+    /**
+     * no annotation document has been created for this document
+     */
+    NEW("NEW"),
+    /**
+     * at least one annotation document has been created for the document
+     */
+    ANNOTATION_IN_PROGRESS("ANNOTATION_INPROGRESS"),
+    /**
+     * all annotations have marked their annotation document as finished
+     */
+    ANNOTATION_FINISHED("ANNOTATION_FINISHED"),
+    /**
+     * curator claims to have curated all annotations
+     */
+    CURATION_FINISHED("CURATION_FINISHED"),
+    /**
+     * curator has started working with the annotation document, annotators can no longer make
+     * modifications in annotation documents
+     */
+    CURATION_IN_PROGRESS("CURATION_INPROGRESS");
+    public String getName()
+    {
+        return getId();
+    }
+    @Override
+    public String toString()
+    {
+        return getId();
+    }
+    SourceDocumentState(String aId)
+    {
+        this.id = aId;
+    }
+
+    private final String id;
+
+    @Override
+    public String getId()
+    {
+        return id;
+    }
+
+}
