8b5c3279f8dd072c7707dbab43f459ccc0fc84b0
Thomas Krause <thomaskrause@posteo.de>
2018-08-01 16:54:45 +0200

    Add a check for multiple ingoing relations for the same target.

---
+++ webanno-diag/src/main/java/de/tudarmstadt/ukp/clarin/webanno/diag/checks/NoMultipleIngoingRelationsCheck.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2016
+ * Ubiquitous Knowledge Processing (UKP) Lab and FG Language Technology
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.tudarmstadt.ukp.clarin.webanno.diag.checks;
+
+import static org.apache.uima.fit.util.CasUtil.getType;
+import static org.apache.uima.fit.util.CasUtil.select;
+import static org.apache.uima.fit.util.CasUtil.selectFS;
+import static org.apache.uima.fit.util.FSUtil.getFeature;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.uima.cas.CAS;
+import org.apache.uima.cas.FeatureStructure;
+import org.apache.uima.cas.Type;
+import org.apache.uima.cas.text.AnnotationFS;
+import org.apache.uima.fit.util.FSUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+
+import de.tudarmstadt.ukp.clarin.webanno.api.AnnotationSchemaService;
+import de.tudarmstadt.ukp.clarin.webanno.api.WebAnnoConst;
+import de.tudarmstadt.ukp.clarin.webanno.diag.CasDoctor.LogLevel;
+import de.tudarmstadt.ukp.clarin.webanno.diag.CasDoctor.LogMessage;
+import de.tudarmstadt.ukp.clarin.webanno.model.AnnotationLayer;
+import de.tudarmstadt.ukp.clarin.webanno.model.Project;
+
+public class NoMultipleIngoingRelationsCheck
+    implements Check
+{
+    private @Autowired AnnotationSchemaService annotationService;
+
+    @Override
+    public boolean check(Project aProject, CAS aCas, List<LogMessage> aMessages)
+    {
+        boolean ok = true;
+        for (AnnotationLayer layer : annotationService.listAnnotationLayer(aProject)) {
+            
+        	if (!WebAnnoConst.RELATION_TYPE.equals(layer.getType())) {
+                continue;
+            }
+
+            Type type;
+            try {
+                type = getType(aCas, layer.getName());
+            }
+            catch (IllegalArgumentException e) {
+                // If the type does not exist, the CAS has not been upgraded. In this case, we
+                // can skip checking the layer because there will be no annotations anyway.
+                continue;
+            }
+            
+            // Remember all nodes that already have a known incoming relation.
+            // Map from the target to the existing source, so the source can be used
+            // to provide a better debugging output.
+            Map<AnnotationFS, AnnotationFS> incoming = new HashMap<>();
+            
+            for (AnnotationFS rel : select(aCas, type)) {
+            	
+            	AnnotationFS source = getFeature(rel, WebAnnoConst.FEAT_REL_SOURCE,
+                        AnnotationFS.class);
+            	AnnotationFS target = getFeature(rel, WebAnnoConst.FEAT_REL_TARGET,
+                        AnnotationFS.class);
+            	
+            	AnnotationFS existingSource = incoming.get(target);
+            	if(existingSource != null) {
+            		target.getCoveredText();
+            		aMessages.add(new LogMessage(this, LogLevel.ERROR,
+                            "Relation [%s] -> [%s] points to entitity that already has an "
+                            + "incoming relation [%s] ->[%s].",
+                            source.getCoveredText(), target.getCoveredText(), 
+                            existingSource.getCoveredText(), target.getCoveredText()));
+                    ok = false;
+            	} else {
+            		incoming.put(target, source);
+            	}
+            }
+        }
+        
+        return ok;
+    }
+}
