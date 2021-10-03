3071469743bb5e26f28eebc3a9badf5bc544e031
Pranay Raj <pranaynallanagula@gmail.com>
2021-10-03 14:05:28 +0200

    unused PROP_TIMESTAMP private field is removed.

--- webanno-remote/src/main/java/de/tudarmstadt/ukp/clarin/webanno/webapp/remoteapi/aero/AeroRemoteApiController.java
+++ webanno-remote/src/main/java/de/tudarmstadt/ukp/clarin/webanno/webapp/remoteapi/aero/AeroRemoteApiController.java
@@ -147,7 +147,7 @@ public class AeroRemoteApiController
     private static final String PROP_NAME = "name";
     private static final String PROP_STATE = "state";
     private static final String PROP_USER = "user";
-    private static final String PROP_TIMESTAMP = "user";
+    
 
     private static final String FORMAT_DEFAULT = "text";
 
