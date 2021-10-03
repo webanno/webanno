256a1105c49c42ae68de5b5f114d715868478021
Pranay Raj <pranaynallanagula@gmail.com>
2021-10-03 13:59:55 +0200

    unused PROP_STATE private field is removed.

--- webanno-remote/src/main/java/de/tudarmstadt/ukp/clarin/webanno/webapp/remoteapi/aero/AeroRemoteApiController.java
+++ webanno-remote/src/main/java/de/tudarmstadt/ukp/clarin/webanno/webapp/remoteapi/aero/AeroRemoteApiController.java
@@ -145,7 +145,7 @@ public class AeroRemoteApiController
 
     private static final String PROP_ID = "id";
     private static final String PROP_NAME = "name";
-    private static final String PROP_STATE = "state";
+    
     private static final String PROP_USER = "user";
     private static final String PROP_TIMESTAMP = "user";
 
